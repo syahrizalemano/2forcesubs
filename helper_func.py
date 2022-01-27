b6ef18bc6b97f32d1f1e70df1170257cb89df956
syahrizalemano <89107294+syahrizalemano@users.noreply.github.com>
2022-01-27 09:25:05 +0800

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -4,7 +4,7 @@ import base64
 import re
 import asyncio
 from pyrogram import filters
-from config import FORCE_SUB_CHANNEL, ADMINS
+from config import FORCE_SUB_CHANNEL, FORCE_SUB_CHANNEL2, ADMINS
 from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
 from pyrogram.errors import FloodWait
 

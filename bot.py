765f724e3641be863451ce19417498a1a395b527
syahrizalemano <89107294+syahrizalemano@users.noreply.github.com>
2022-01-27 08:55:51 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -24,16 +24,14 @@ class Bot(Client):
         await super().start()
         usr_bot_me = await self.get_me()
 
-        if FORCE_SUB_CHANNEL,:
-           FORCE_SUB_CHANNEL2:
+        if FORCE_SUB_CHANNEL, FORCE_SUB_CHANNEL2:
             try:
-                link = await self.export_chat_invite_link(FORCE_SUB_CHANNEL),
-                                                         (FORCE_SUB_CHANNEL2)
+                link = await self.export_chat_invite_link(FORCE_SUB_CHANNEL), (FORCE_SUB_CHANNEL2)
                 self.invitelink = link
             except Exception as a:
                 self.LOGGER(__name__).warning(a)
                 self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
-                self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_CHANNEL value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL}")
+                self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_CHANNEL value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL, FORCE_SUB_CHANNEL2}")
                 self.LOGGER(__name__).info("\nBot Stopped.")
                 sys.exit()
         try:
